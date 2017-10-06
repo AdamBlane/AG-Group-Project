0227010ea7fe531b5b91190f39214f4c6904fe0a
adam blance <adam.blance@btinternet.com>
2017-10-06 12:31:57 +0100

    need to combine with v

--- 98-engine-simple/main.cpp
+++ 98-engine-simple/main.cpp
@@ -19,8 +19,7 @@ static void key_callback(GLFWwindow* win, int key, int scancode, int action, int
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(win, GL_TRUE);
 }
-
-bool render() 
+void drawscene()
 {
 	float ratio;
 	int width, height;
@@ -31,16 +30,11 @@ bool render()
 	glViewport(0, 0, width, height);
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	/*glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-	glMatrixMode(GL_MODELVIEW);*/
-
-	glLoadIdentity();
-	//glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.0f);
-	auto V = cam.get_View();
-	auto P = cam.get_Projection();
-
+	glMatrixMode(GL_MODELVIEW); 
 	glBegin(GL_TRIANGLES);
 	glColor3f(1.f, 0.f, 0.f);
 	glVertex3f(-0.6f, -0.4f, 0.f);
@@ -48,34 +42,27 @@ bool render()
 	glVertex3f(0.6f, -0.4f, 0.f);
 	glColor3f(0.f, 0.f, 1.f);
 	glVertex3f(0.f, 0.6f, 0.f);
+
+
 	glEnd();
-
-	auto M = GL_MODELVIEW_MATRIX;
-	auto MVP = P * V;
-	MVP *= M;
-
-	if (glfwGetKey(GLFW_KEY_W))
-		pos = (vec3(0.0f, 0.0f, 2.0f));
-	if (glfwGetKey(GLFW_KEY_A))
-		pos = (vec3(-2.0f, 0.0f, 0.0f));
-	if (glfwGetKey(renderer::get_window(), GLFW_KEY_S))
-		pos = (vec3(0.0f, 0.0f, -2.0f));
-	if (glfwGetKey(renderer::get_window(), GLFW_KEY_D))
-		pos = (vec3(2.0f, 0.0f, 0.0f));
-
-	return true;
 }
+
 void update() 
 {
+	glRotatef(cam.get_pitch(), 1.0f, .0f, .0f);
+	glRotatef(cam.get_yaw(), .0f, 1.0f, .0f);
+	glTranslatef(-(cam.get_Posistion().x + 2), -(cam.get_Posistion().y), -(cam.get_Posistion().z));
+	drawscene();
 
 }
 int main(int argc, char **argv)
 {
-	cam.set_Posistion(glm::vec3(0.0f, 10.0f, 0.0f));
+	cam.set_Posistion(glm::vec3(0.0f, 0.0f, 0.0f));
 	cam.set_Target(vec3(0.0f, 0.0f, 0.0f));
 	auto aspect = static_cast<float>(640 / 480);
 	cam.set_projection(quarter_pi<float>(), aspect, 2.414f, 1000.0f);
-
+	
+	
 	// Set error callbackc function
 	glfwSetErrorCallback(error_callback);
 	// Initialise GLFW
@@ -83,6 +70,7 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	// Initialise the window, check for error after
 	window = glfwCreateWindow(640, 480, "Triangle Simulator 2017", NULL, NULL);
+
 	if (!window)
 	{
 		glfwTerminate();
@@ -92,11 +80,10 @@ int main(int argc, char **argv)
 	glfwMakeContextCurrent(window);
 	// Assign key callback function for close on ESC
 	glfwSetKeyCallback(window, key_callback);
-
 	// While window is not to be closed...
 	while (!glfwWindowShouldClose(window))
 	{
-		render();
+		update();
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
